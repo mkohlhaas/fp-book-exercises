44e7182e2ed7dabfdb593162aee0f3c57eda793f
Hans-J. Schmid <knock@myopendoor.de>
2022-01-03 18:01:01 +0100

    refines Ch07a2.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Possible Solutions:
 - [Chapter 5](https://gist.github.com/mkohlhaas/972b0efdb0af3a02bea1b90abd5acc43)
 - [Chapter 7a1](https://gist.github.com/mkohlhaas/829127bad61a00c455bc1a67addb7487)
+- [Chapter 7a2](https://gist.github.com/mkohlhaas/3afdb95bbccabbec71dcfc2a6085d727)
--- src/Ch07a2.purs
+++ src/Ch07a2.purs
@@ -1,31 +1,28 @@
 module Ch07a2 where
 
-import Prelude        (Unit, discard, (<>))
-import Effect         (Effect)
-import Effect.Console (log)
-
--------------------- JS Primitives --------------------------------------------------------
-foreign import ordIntImpl     :: Ordering -> Ordering -> Ordering -> Int -> Int -> Ordering
-foreign import eqIntImpl      :: Int -> Int -> Boolean
-foreign import showIntImpl    :: Int -> String
-foreign import showStringImpl :: String -> String
+import Prelude           (Unit)
+-- import Data.Eq           (class Eq)
+-- import Data.Ord          (class Ord)
+-- import Data.Show         (class Show)
+import Effect            (Effect)
+import Effect.Console    (log)
 
 -------------------- Tests ----------------------------------------------------------------
 test :: Effect Unit
 test = do
-  log "Uncomment each line. DERIVE missing functions !!! No further imports!"
-  -- log $ show $ Just 5 == Just 5                            -- true
-  -- log $ show $ Just 5 == Just 2                            -- false
-  -- log $ show $ Just 5 == Nothing                           -- false
+  log "Uncomment each line. DERIVE missing functions !!!"
+  -- log $ show $ Just 5  == Just 5                           -- true
+  -- log $ show $ Just 5  == Just 2                           -- false
+  -- log $ show $ Just 5  == Nothing                          -- false
   -- log $ show $ Nothing == Just 5                           -- false
   -- log $ show $ Nothing == Nothing                          -- true
   -- log $ show $ Left "left"                                 -- (Left "left")
   -- log $ show $ Right (Just 42)                             -- (Right (Just 42))
-  -- log $ show $ Just 1 < Just 5                             -- true
-  -- log $ show $ Just 5 <= Just 5                            -- true
-  -- log $ show $ Just 5 > Just 10                            -- false
+  -- log $ show $ Just 1  <  Just 5                           -- true
+  -- log $ show $ Just 5  <= Just 5                           -- true
+  -- log $ show $ Just 5  >  Just 10                          -- false
   -- log $ show $ Just 10 >= Just 10                          -- true
-  -- log $ show $ Just 99 > Nothing                           -- true
-  -- log $ show $ Just 99 < Nothing                           -- false
+  -- log $ show $ Just 99 >  Nothing                          -- true
+  -- log $ show $ Just 99 <  Nothing                          -- false
   -- log $ show $ Just "abc"                                  -- (Just "abc")
-  -- log $ show $ Nothing                                     -- Nothing
+  -- log $ show $ (Nothing :: Maybe Unit)                     -- Nothing
