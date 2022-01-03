43091743a26d2d108e96a7dce634b0d03221e2ee
Hans-J. Schmid <knock@myopendoor.de>
2022-01-03 21:21:43 +0100

    reformats Ch07b.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Possible Solutions:
 - [Chapter 5](https://gist.github.com/mkohlhaas/972b0efdb0af3a02bea1b90abd5acc43)
 - [Chapter 7a1](https://gist.github.com/mkohlhaas/829127bad61a00c455bc1a67addb7487)
 - [Chapter 7a2](https://gist.github.com/mkohlhaas/3afdb95bbccabbec71dcfc2a6085d727)
+- [Chapter 7b](https://gist.github.com/mkohlhaas/a936771698944e6242306ec73437f75b)
--- src/Ch07b.purs
+++ src/Ch07b.purs
@@ -4,14 +4,13 @@ import Prelude        (Unit)
 import Effect         (Effect)
 import Effect.Console (log)
 
-newtype CSV = CSV String
-
 class ToCSV a where
   toCSV :: a -> CSV
 
--- class FromCSV a where
---   fromCSV :: CSV -> Maybe a
+class FromCSV a where
+  fromCSV :: CSV -> Maybe a
 
+newtype CSV         = CSV String
 newtype FullName    = FullName String
 newtype Age         = Age Int
 data    Occupation  = Doctor | Dentist | Lawyer | Unemployed
@@ -19,8 +18,8 @@ data    Person      = Person { name :: FullName , age :: Age , occupation :: Occ
 
 test :: Effect Unit
 test = do
-  log "Uncomment lines step by step. Implement/import missing functions and all the rest ..."
-  -- log $ show $ toCSV (Person { name: FullName "Sue Smith" , age: Age 23 , occupation: Doctor })
+  log "Uncomment each line. Implement/import missing functions and all the rest ..."
+  -- log $ show $ toCSV (Person { name: FullName "Sue Smith" , age: Age 23 , occupation: Doctor }) -- "Sue Smith,23,Doctor"
   -- log $ show $ toCSV (Person { name: FullName "Sue Smith" , age: Age 23 , occupation: Doctor }) == CSV "Sue Smith,23,Doctor"
   -- let person = Person { name: FullName "Sue Smith" , age: Age 23 , occupation: Doctor }
   -- log $ show $ (toCSV person # fromCSV) == Just person
