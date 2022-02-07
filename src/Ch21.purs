29dbe8a9edc443efa1c5f83fa5bc7052be506750
Hans-J. Schmid <knock@myopendoor.de>
2022-02-07 02:50:54 +0100

    finished StateT monad.

--- src/Ch21.purs
+++ src/Ch21.purs
@@ -12,19 +12,26 @@ newtype StateT s m a = StateT (s -> m (Tuple a s))
 runStateT :: ∀ s m a. StateT s m a -> s -> m (Tuple a s)
 runStateT (StateT f) = f
 
--- functor
+-- Functor
 instance functorStateT :: Functor m => Functor (StateT s m) where
   map f (StateT g) = StateT \s -> map (\(Tuple a b) -> Tuple (f a) b) (g s)
 
--- apply
--- instance applyStateT :: Apply (StateT s m) where
---   apply (StateT f) (StateT g) = StateT \s -> map ((\(Tuple h s1) -> (\(Tuple x s2) -> Tuple (h x) s2)) (f s)) (g s)
+-- Apply
+instance applyStateT :: Monad m => Apply (StateT s m) where
+  apply = ap
 
--- applicative
+-- Applicative
+instance applicativeStateT :: Monad m => Applicative (StateT s m) where
+  pure x = StateT \s -> pure $ Tuple x s
 
--- bind
+-- Bind
+instance bindStateT :: Monad m => Bind (StateT s m) where
+  -- bind :: m a -> (a -> m b) -> m b
+  -- bind :: StateT s m a -> (a -> StateT s m b) -> StateT s m b
+  bind (StateT x) f = StateT \s -> x s >>= \(Tuple x1 s1) -> case f x1 of StateT f2 -> f2 s1
 
--- monad
+-- Monad
+instance monadStateT :: Monad m => Monad (StateT s m)
 
 test :: Effect Unit
 test = do
